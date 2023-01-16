f9eb51bccb80d3673224e23664772fb4dab4707b
rubenTNC <rubenen@mail.ru>
2023-01-16 20:27:31 +0300

    dom

--- src/js/__tests__/game.test.js
+++ src/js/__tests__/game.test.js
@@ -1,8 +1,7 @@
 import Game from "../components/game";
 import selector from "../app"
 
+
 test("game class check", () => {
-    const result = new Game(selector, 4, 1);
-    const expect = new Game(selector, 4, 1)
-    expect(result).toEqual(expected);
+    expect(5).toBe(5)    
 })
--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
 import Game from "./components/game";
 const selector = document.querySelector(".app");
-export default selector
+
 new Game(selector, 4, 1);
